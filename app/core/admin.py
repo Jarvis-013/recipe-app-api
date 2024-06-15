423ae3d8a3cfc1409a7087e8ceb5686cac952148
Jarvis-013 <rajatbhaskar001@gmail.com>
2024-06-15 16:41:13 +0530

    changes

--- app/core/admin.py
+++ app/core/admin.py
@@ -3,9 +3,10 @@ Django admin customization.
 """
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-from django.utils.translation import gettext_lazy as _
 
+# Register your models here.
 from core import models
+from django.utils.translation import gettext_lazy as _
 
 
 class UserAdmin(BaseUserAdmin):
--- app/core/models.py
+++ app/core/models.py
@@ -14,7 +14,6 @@ class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
         """Create, save and return a new user."""
-
         if not email:
             raise ValueError('User must have an email address.')
         user = self.model(email=self.normalize_email(email), **extra_fields)
